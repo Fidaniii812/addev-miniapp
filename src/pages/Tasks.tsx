5bffcb1100a293aa9959ef933f0df919c1f96c37
Fidaniii812 <fidanbeciri1982@gmail.com>
2026-07-25 13:51:42 +0200

    Create Tasks.tsx

---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,13 @@
+export default function Tasks() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "40px 20px",
+      }}
+    >
+      <h2>Tasks</h2>
+      <p>Complete tasks and earn AdDev Points.</p>
+    </div>
+  );
+}
