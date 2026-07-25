6dcc8f5f9e23c3a5421df2681a2cadcca403608d
Fidaniii812 <fidanbeciri1982@gmail.com>
2026-07-25 13:40:26 +0200

    Add Home component for AdDev Rewards

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "40px 20px",
+      }}
+    >
+      <h2>Welcome to AdDev Rewards</h2>
+      <p>Development Version</p>
+    </div>
+  );
+}
