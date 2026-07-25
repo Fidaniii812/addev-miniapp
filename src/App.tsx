07c5a27e001578205c4dcae000c49756a81ed26b
Fidaniii812 <fidanbeciri1982@gmail.com>
2026-07-25 15:06:59 +0200

    Import useEffect and initialize Telegram
    Added useEffect to initialize Telegram on app load.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import Layout from "./components/Layout";
-
 import Home from "./pages/Home";
 import Tasks from "./pages/Tasks";
 import Wallet from "./pages/Wallet";
 import Community from "./pages/Community";
 import Profile from "./pages/Profile";
+import { initializeTelegram } from "./lib/telegram";
 
 export default function App() {
   const [page, setPage] = useState("home");
+
+  useEffect(() => {
+    // Inicializon Telegram WebApp automatikisht
+    initializeTelegram();
+  }, []);
 
   return (
     <Layout currentPage={page} onChangePage={setPage}>
