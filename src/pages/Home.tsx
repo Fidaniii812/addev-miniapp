a84ee014a348dd82e33958c7c536bba5791ad4e4
Fidaniii812 <fidanbeciri1982@gmail.com>
2026-07-25 17:11:03 +0200

    Refactor buy and spend actions in Home component
    Refactor buy and spend handlers to use dedicated functions and add transaction logging.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
 import UserCard from "../components/UserCard";
 import TelegramStarsCard from "../components/TelegramStarsCard";
+import StarsBalance from "../components/StarsBalance";
+import StarsActions from "../components/StarsActions";
+import TransactionHistory from "../components/TransactionHistory";
+import StarsStats from "../components/StarsStats";
+import StarsOverview from "../components/StarsOverview";
 
 import useTelegramUser from "../hooks/useTelegramUser";
 import useTelegramStars from "../hooks/useTelegramStars";
+import useTelegramTransactions from "../hooks/useTelegramTransactions";
+import useStarsStats from "../hooks/useStarsStats";
 
 export default function Home() {
   const user = useTelegramUser();
@@ -13,14 +20,57 @@ export default function Home() {
     spendStars,
   } = useTelegramStars();
 
+  const {
+    transactions,
+    addTransaction,
+  } = useTelegramTransactions();
+
+  const {
+    totalBought,
+    totalSpent,
+  } = useStarsStats(transactions);
+
+  const handleBuy = () => {
+    buyStars(10);
+    addTransaction("buy", 10);
+  };
+
+  const handleSpend = () => {
+    spendStars(10);
+    addTransaction("spend", 10);
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <UserCard user={user} />
 
       <TelegramStarsCard
         stars={stars}
-        onBuy={() => buyStars(10)}
-        onSpend={() => spendStars(10)}
+        onBuy={handleBuy}
+        onSpend={handleSpend}
+      />
+
+      <StarsBalance stars={stars} />
+
+      <StarsActions
+        onBuy={handleBuy}
+        onSpend={handleSpend}
+      />
+
+      <StarsStats
+        stars={stars}
+        totalBought={totalBought}
+        totalSpent={totalSpent}
+      />
+
+      <StarsOverview
+        stars={stars}
+        totalBought={totalBought}
+        totalSpent={totalSpent}
+      />
+
+      <TransactionHistory
+        transactions={transactions}
       />
     </div>
   );
